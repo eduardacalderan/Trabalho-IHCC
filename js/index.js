05b252dccdfad075533174d33edcce7eff01a8ef
Eduarda Calderan <85559533+eduardacalderan@users.noreply.github.com>
2021-08-20 11:30:48 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,51 @@
+/*abrir e fechar menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*esconder o menu*/
+const links = document.querySelectorAll('nav a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // aqui é menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/*back to top  */
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*when scroll */
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
